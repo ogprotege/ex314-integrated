75ece2f0eacb15b4fa8efa78d76efd5c89c739d1
William Sherman <75035839+ogprotege@users.noreply.github.com>
2025-05-05 02:38:22 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    // Server Actions are available by default in Next.js 14+, so we can remove this option
-  },
   images: {
     domains: ['images.unsplash.com']
   }
 }
 
-export default nextConfig;
+module.exports = nextConfig
